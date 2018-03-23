6f561ff1f713a4e9fd22ef8feea9c0b1ac37d367
B.I. Vodenicharski <biv20@pcphy117.phy.pwf.cam.ac.uk>
2018-03-23 18:49:38 +0000

    Add true init commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+
+class Loader:
+	def __init__(self)
+		#TODO Set up dataFrame for each file
+
+
+class LinSolver:
+
+	def __init__(self):
+	""" This class handles the solution of a large set of linear equations, and
+	    and provides functionality for error analysis.  """
+		pass
+
+	def 
