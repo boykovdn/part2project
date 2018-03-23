44376b64a1aefdd52523c7bee12ad8bd6e5c83a6
B.I. Vodenicharski <biv20@pcphy117.phy.pwf.cam.ac.uk>
2018-03-23 19:37:42 +0000

    Update to load raw data

--- main.py
+++ main.py
@@ -3,15 +3,28 @@ import numpy as np
 
 
 class Loader:
-	def __init__(self)
+	def __init__(self):
 		#TODO Set up dataFrame for each file
+		self.dataframe = pd.DataFrame.from_csv("./data/2012_S1W2.csv")  # Change if data not locally available
+		
 
 
 class LinSolver:
 
 	def __init__(self):
-	""" This class handles the solution of a large set of linear equations, and
-	    and provides functionality for error analysis.  """
+		""" This class handles the solution of a large set of linear equations, and
+	    	    and provides functionality for error analysis.  """
 		pass
 
-	def 
+	def parse_matrix_coef_from_data(self, dataframe):
+		""" Calculates coefficients of linear equations from slice of original data.  """ 
+	
+
+
+def main():
+	l = Loader()
+	print(l.dataframe.head())
+
+
+if __name__ == "__main__":
+	main()
