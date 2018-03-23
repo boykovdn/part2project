cecd4ae481d12b04098a41b04235296a0bbb40ec
B.I. Vodenicharski <biv20@pcphy117.phy.pwf.cam.ac.uk>
2018-03-23 19:59:30 +0000

    Add method for getting LinSolver dataframe

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ import numpy as np
 
 class Loader:
 	def __init__(self):
-		#TODO Set up dataFrame for each file
+		""" This class loads raw data and slices it in ways so as to be passed to the 
+		    rest of the program.  """
 		self.dataframe = pd.DataFrame.from_csv("./data/2012_S1W2.csv")  # Change if data not locally available
-		
+	
+	def get_linsolver_coef_raw_dataframe(self):
+		""" Picks subset of data to be passed to LinSolver. """
+		dataframe_result = pd.DataFrame()
+		dataframe_result["az"] = self.dataframe["az"]
+		dataframe_result["el"] = self.dataframe["el"]
+		dataframe_result["delay1"] = self.dataframe["delay1"]
+		dataframe_result["delay2"] = self.dataframe["delay2"]
+		dataframe_result.reset_index(drop=True, inplace=True)
+
+		return dataframe_result
 
 
 class LinSolver:
@@ -18,12 +29,15 @@ class LinSolver:
 
 	def parse_matrix_coef_from_data(self, dataframe):
 		""" Calculates coefficients of linear equations from slice of original data.  """ 
+		#TODO Calculate indices
+
+	#TODO Write matrix inversion script, get some initial results
 	
 
 
 def main():
 	l = Loader()
-	print(l.dataframe.head())
+	print(l.get_linsolver_coef_raw_dataframe().head())
 
 
 if __name__ == "__main__":
